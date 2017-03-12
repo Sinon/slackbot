f90cb9713d24b5e28a054c80312eb11c513fe981
Rob <rob.h.1988@gmail.com>
2017-03-12 20:38:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,82 @@
+import os
+import time
+from datetime import datetime
+import asyncio
+import json
+import sys
+
+import aiohttp
+
+from slackclient import SlackClient
+from flask import Flask, request, Response
+
+
+slack_token = os.environ["SLACK_API_TOKEN"]
+SLACK_WEBHOOK_SECRET = os.environ.get('SLACK_WEBHOOK_SECRET')
+sc = SlackClient(slack_token)
+
+# [{'type': 'message',
+# 'channel': 'C4H2ZJVUK',
+# 'user': 'U4HRBK2QP', 'text': 'hello test bot',
+# 'ts': '1489331005.529033', 'source_team': 'T4H2ZJVA7', 'team': 'T4H2ZJVA7'}]
+
+
+# [{'text': 'Testing :tada:', 'username': 'Slack API Tester',
+# 'bot_id': 'B4H33G00K', 'type': 'message', 'subtype': 'bot_message',
+# 'team': 'T4H2ZJVA7', 'channel': 'C4H2ZJVUK', 'event_ts': '1489330956.526758', 'ts': '1489330956.526758'}]
+
+
+def rtm_loop(start_time):
+    while True:
+        events = sc.rtm_read()
+        for e in events:
+            print(e)
+            e_type = e.get('type')
+            is_bot = bool(e.get('bot_id'))
+            new_message = e.get('ts') and float(e['ts']) > start_time
+            e_text = e.get('text')
+            parrot_rmt_messages(e_text, e_type, is_bot, new_message)
+        time.sleep(1)
+
+
+def parrot_rmt_messages(text, e_type, is_bot, new_message):
+    if e_type and e_type == 'message' and not is_bot and new_message:
+        # posts as bot?
+        resp = sc.api_call('chat.postMessage', channel='#general', text=text)
+        if not resp['ok']:
+            print(resp)
+            # posts as user?
+            # sc.rtm_send_message("general", e['text'], username='testbot)
+
+
+def list_channels():
+    channels_call = sc.api_call("channels.list")
+    if channels_call.get('ok'):
+        return channels_call['channels']
+    return None
+
+
+
+def main():
+    print(sc.api_call("auth.test"))
+
+    print(sc.__dict__)
+    sc.api_call(
+        "chat.postMessage",
+        channel="#general",
+        text="Testing, bot has been started :tada:",
+        username='testbot'
+    )
+    if sc.rtm_connect():
+        start_time = time.time()
+        rtm_loop(start_time)
+    else:
+        print('Connection failed')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    main()
+
+
+
---
+++ server.py
@@ -0,0 +1,68 @@
+import os
+
+from flask import Flask, request, jsonify, Response
+import requests
+
+
+app = Flask(__name__)
+
+SLACK_WEBHOOK_SECRET = os.environ.get('SLACK_WEBHOOK_SECRET')
+
+
+def slack_resp(text, response_type='in_channel'):
+    return {'text': text, 'response_type': response_type}
+
+
+@app.route('/parrot', methods=['POST'])
+def inbound():
+    print(request.form)
+    channel = request.form.get('channel_name')
+    username = request.form.get('user_name')
+    text = request.form.get('text')
+    inbound_message = username + " in " + channel + " says: " + text
+    return jsonify(slack_resp(inbound_message))
+
+valid_build_job_name = ['api']
+
+
+@app.route('/build', methods=['POST'])
+def build():
+    print(request.form)
+    username = request.form.get('user_name')
+    text = request.form.get('text')
+    response_url = request.form['response_url']
+    if text not in valid_build_job_name:
+        text = 'Unknown job {} supplied. Valid options: {}'.format(text, valid_build_job_name)
+        response_type = 'ephemeral'
+        return jsonify(slack_resp(text, response_type))
+    text = 'Build {} has been started by {}'.format(text, username)
+    requests.post(response_url, json=slack_resp(text))
+    # TODO Simulate a long job with message when build has "finished"
+    requests.post(response_url, json=slack_resp('Build has finished.'))
+    return Response(), 200
+
+
+@app.route('/deploy', methods=['POST'])
+def deploy():
+    print(request.form)
+    text = request.form['text']
+    text_tokenised = text.split(' ')
+    response_url = request.form['response_url']
+    if len(text_tokenised) != 2:
+        text = ('Incorrect number of parameters supplied ({}).'
+                ' /deploy [branch] [environment]'.format(len(text_tokenised)))
+        response_type = 'ephemeral'
+        return jsonify(slack_resp(text, response_type))
+
+    git_b, environment = text.split(' ')
+    requests.post(response_url, json={"response_type": "in_channel"})
+    text = 'Deploying branch {} to {}'.format(git_b, environment)
+    requests.post(response_url, json=slack_resp(text))
+    # TODO Simulate a long job with message when deploy has "finished"
+    requests.post(response_url, json=slack_resp('Deploy has finished!'))
+    return Response(), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
